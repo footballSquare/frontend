d580c4cc83b78501a4faf4acf4c0b1aa14eba75c
RyuDongHo <thffhahs03@gmail.com>
2025-02-23 22:36:38 +0900

    feat: apply zustand on nav btns

--- src/0_App/ui/Nav/ui/NavigationBtn/index.tsx
+++ src/0_App/ui/Nav/ui/NavigationBtn/index.tsx
@@ -1,10 +1,12 @@
 import { NavigationBtnProps } from "./type";
+import usePageStore from "../../../../../4_Shared/zustand/usePageStore";
 
 const NavigationBtn = (props: NavigationBtnProps) => {
+  const {page, setPage} = usePageStore();
   const {text} = props;
   return (
-    <button className="flex items-center justify-center">
-      <h5 className="lg:text-bold lg:font-bold text-xs font-base">{text}</h5>
+    <button className="flex items-center justify-center" onClick={()=>{setPage(text)}}>
+      <h5 className={`lg:text-bold lg:font-bold text-xs font-base ${page === text && "text-blue"}`}>{text}</h5>
     </button>
   );
 };
