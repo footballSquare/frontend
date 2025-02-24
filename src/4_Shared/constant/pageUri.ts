3a67f59f58677c867b0584c21aa11400428f35af
RyuDongHo <thffhahs03@gmail.com>
2025-02-24 14:45:07 +0900

    chore: make&apply page uri constant

---
+++ src/4_Shared/constant/pageUri.ts
@@ -0,0 +1,14 @@
+const PAGE_URI = {
+  WELCOME: "/",
+  MAIN: "/main",
+  TEAMLIST: "/team-list",
+  TEAM: "/team",
+  COMMUNITY: "/community",
+  RANKING: "/ranking",
+  PROFILE: "/profile",
+  FREEMATCH: "/free-match",
+  LOGIN: "/login",
+  SIGNUP: "/signup",
+};
+
+export default PAGE_URI;
