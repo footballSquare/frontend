c1bcae5c7ccf57d85f9b6b36e228c14df0b3f3c1
RyuDongHo <thffhahs03@gmail.com>
2025-02-25 00:33:42 +0900

    chore: MatchList ui segment in FreeMatch page FreeMatch page의 MatchList ui의 style 일부 수정

--- src/1_Page/FreeMatch/ui/MatchList/index.tsx
+++ src/1_Page/FreeMatch/ui/MatchList/index.tsx
@@ -1,18 +1,29 @@
 import React from "react";
 import MatchCard from "./ui/MatchCard";
 import useGetOpenMatchList from "../../../../3_Entity/Match/useGetOpenMatchList";
-import useInfiniteScrollPaging from "./model/useInfiniteScrollPaging";
+import useInfiniteScrollPaging from "../../../../4_Shared/model/useInfiniteScrollPaging";
 
 const MatchList = () => {
   const [page, setPage] = React.useState<number>(1);
-  console.log(page)
   const [openMatchList, hasMoreContent, loading] = useGetOpenMatchList(page);
-  const [observeRef] = useInfiniteScrollPaging(setPage, loading, hasMoreContent);
+  const [observeRef] = useInfiniteScrollPaging(
+    setPage,
+    loading,
+    hasMoreContent
+  );
 
   return (
     <div className="flex flex-col gap-2 overflow-y-auto h-[70vh]">
       {openMatchList.map((elem, index) => {
-        return <MatchCard {...elem} observeRef={openMatchList.length === index + 1 && observeRef || undefined} />;
+        return (
+          <MatchCard
+            key={index}
+            {...elem}
+            observeRef={
+              (openMatchList.length === index + 1 && observeRef) || undefined
+            }
+          />
+        );
       })}
     </div>
   );
