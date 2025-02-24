614d5febc17a14f8d28e97c1a3bc5c919487b8e3
RyuDongHo <thffhahs03@gmail.com>
2025-02-24 14:05:27 +0900

    feat: Welcome page welcome page를 추가했습니다

---
+++ src/1_Page/WelcomePage/index.tsx
@@ -0,0 +1,40 @@
+import welcomeBanner from "./assets/svg/welcomeBanner.svg";
+import { useNavigate } from "react-router-dom";
+const WelcomePage = () => {
+  const navigate = useNavigate();
+  return (
+    <div className="flex">
+      <div className="flex flex-col">
+        <p className=" font-bold text-6xl mt-[100px]">
+          MAKE ONLY
+          <br />
+          YOUR PLAY
+          <br />
+          WITH
+          <br />
+          FOOTBALL SQUARE
+          <p className=" text-base font-light m-4 ml-0">
+            팀을 만들고, 대회에 참가하세요!
+          </p>
+        </p>
+        <button
+          className="border-1 w-[180px] h-[56px] rounded-2xl bg-black text-white"
+          onClick={() => {
+            navigate("/login");
+          }}
+        >
+          Get Start
+        </button>
+      </div>
+      <div className="mt-[90px]">
+        <img
+          src={welcomeBanner}
+          className=" max-w-[800px]"
+          alt="welcome_banner"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default WelcomePage;
--- src/1_Page/index.tsx
+++ src/1_Page/index.tsx
@@ -1,11 +1,11 @@
-import Main from "./Main";
+import WelcomePage from "./WelcomePage";
 import { Routes, Route } from "react-router-dom";
 
 const Page = () => {
   return (
     <div className=" w-full flex justify-center items-center">
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={<WelcomePage />} />
       </Routes>
     </div>
   );
