a9498037f4e82ec62179d8ec0fba71ba7c54db66
RyuDongHo <thffhahs03@gmail.com>
2025-02-25 00:35:49 +0900

    chore: add svg & mock data & constant FreeMatch page에서 필요한 svg & mock data & constant 추가

---
+++ src/4_Shared/mock/waitingPlayerList.ts
@@ -0,0 +1,74 @@
+export const waitingPlayerList = {
+  "access_list": [
+    {
+      "player_list_idx": 30,
+      "player_list_platform": 0,
+      "player_list_profile_img": "temp",
+      "player_list_nickname": "김지단",
+      "player_prefer_position": "공격수"
+    },
+    {
+      "player_list_idx": 44,
+      "player_list_platform": 1,
+      "player_list_profile_img": "temp",
+      "player_list_nickname": "최라모스",
+      "player_prefer_position": "수비수",
+    },
+    {
+      "player_list_idx": 45,
+      "player_list_platform": 2,
+      "player_list_profile_img": "temp",
+      "player_list_nickname": "박지성",
+      "player_prefer_position": "수비수"
+    },
+    {
+      "player_list_idx": 46,
+      "player_list_platform": 1,
+      "player_list_profile_img": "temp",
+      "player_list_nickname": "손흥민",
+      "player_prefer_position": "미드필더"
+    },
+    {
+      "player_list_idx": 47,
+      "player_list_platform": 0,
+      "player_list_profile_img": "temp",
+      "player_list_nickname": "이강인",
+      "player_prefer_position": "미드필더"
+    },
+    {
+      "player_list_idx": 48,
+      "player_list_platform": 1,
+      "player_list_profile_img": "temp",
+      "player_list_nickname": "차범근",
+      "player_prefer_position": "미드필더"
+    },
+    {
+      "player_list_idx": 49,
+      "player_list_platform": 2,
+      "player_list_profile_img": "temp",
+      "player_list_nickname": "황희찬",
+      "player_prefer_position": "골키퍼"
+    },
+    {
+      "player_list_idx": 50,
+      "player_list_platform": 2,
+      "player_list_profile_img": "temp",
+      "player_list_nickname": "김민재",
+      "player_prefer_position": "공격수"
+    },
+    {
+      "player_list_idx": 51,
+      "player_list_platform": 0,
+      "player_list_profile_img": "temp",
+      "player_list_nickname": "정우영",
+      "player_prefer_position": "골키퍼"
+    },
+    {
+      "player_list_idx": 52,
+      "player_list_platform": 1,
+      "player_list_profile_img": "temp",
+      "player_list_nickname": "이승우",
+      "player_prefer_position": "공격수"
+    }
+  ]
+}
