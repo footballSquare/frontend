ed402d11236d3e156ad34205a780053aac956c20
RyuDongHo <thffhahs03@gmail.com>
2025-02-24 14:43:37 +0900

    feat: navigation btn router apply nav ui의 navigation btn을 통해 page routing이 가능하도록 수정

--- src/0_App/ui/Nav/index.tsx
+++ src/0_App/ui/Nav/index.tsx
@@ -1,12 +1,20 @@
 import logo from "../../../4_Shared/assets/svg/logo.svg";
+import PAGE_URI from "../../../4_Shared/constant/pageUri";
 import NavigationBtn from "./ui/NavigationBtn";
 import SignBtns from "./ui/SignBtns";
+import { useNavigate } from "react-router-dom";
 const Nav = () => {
+  const navigate = useNavigate();
   return (
     <nav className="flex h-[80px] w-full justify-center items-center">
       <div className="sm:justify-between flex justify-center max-w-[1280px] w-full items-center">
         {/* Logo */}
-        <button className="lg:flex hidden items-center justify-center">
+        <button
+          className="lg:flex hidden items-center justify-center"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
           <img
             className="lg:min-w-[120px] w-[120px]"
             src={logo}
@@ -15,12 +23,42 @@ const Nav = () => {
         </button>
         {/* Navigation Btns */}
         <div className="lg:gap-[30px] gap-3 flex items-center justify-between ">
-          <NavigationBtn text={"TEAM LIST"} />
-          <NavigationBtn text={"MY TEAM"} />
-          <NavigationBtn text={"FIND MATCH"} />
-          <NavigationBtn text={"COMMUNITY"} />
-          <NavigationBtn text={"RANKING"} />
-          <NavigationBtn text={"MY PROFILE"} />
+          <NavigationBtn
+            text={"TEAM LIST"}
+            navigationHandler={() => {
+              navigate(PAGE_URI.TEAMLIST);
+            }}
+          />
+          <NavigationBtn
+            text={"MY TEAM"}
+            navigationHandler={() => {
+              navigate(PAGE_URI.TEAM);
+            }}
+          />
+          <NavigationBtn
+            text={"FIND MATCH"}
+            navigationHandler={() => {
+              navigate(PAGE_URI.FREEMATCH);
+            }}
+          />
+          <NavigationBtn
+            text={"COMMUNITY"}
+            navigationHandler={() => {
+              navigate(PAGE_URI.COMMUNITY);
+            }}
+          />
+          <NavigationBtn
+            text={"RANKING"}
+            navigationHandler={() => {
+              navigate(PAGE_URI.RANKING);
+            }}
+          />
+          <NavigationBtn
+            text={"MY PROFILE"}
+            navigationHandler={() => {
+              navigate(PAGE_URI.PROFILE);
+            }}
+          />
         </div>
         {/* SignUp/out LogIn/Out Btns */}
         <SignBtns />
--- src/0_App/ui/Nav/ui/NavigationBtn/index.tsx
+++ src/0_App/ui/Nav/ui/NavigationBtn/index.tsx
@@ -2,11 +2,24 @@ import { NavigationBtnProps } from "./type";
 import usePageStore from "../../../../../4_Shared/zustand/usePageStore";
 
 const NavigationBtn = (props: NavigationBtnProps) => {
-  const {page, setPage} = usePageStore();
-  const {text} = props;
+  const { text, navigationHandler } = props;
+  const { page, setPage } = usePageStore();
+
   return (
-    <button className="flex items-center justify-center" onClick={()=>{setPage(text)}}>
-      <h5 className={`lg:text-bold lg:font-bold text-xs font-base ${page === text && "text-blue"}`}>{text}</h5>
+    <button
+      className="flex items-center justify-center"
+      onClick={() => {
+        setPage(text);
+        navigationHandler();
+      }}
+    >
+      <h5
+        className={`lg:text-bold lg:font-bold text-xs font-base ${
+          page === text && "text-blue"
+        }`}
+      >
+        {text}
+      </h5>
     </button>
   );
 };
