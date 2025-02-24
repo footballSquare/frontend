93a3caee372032a7a822ae6ba7816112452fd57b
RyuDongHo <thffhahs03@gmail.com>
2025-02-24 14:45:07 +0900

    chore: make&apply page uri constant

--- src/1_Page/index.tsx
+++ src/1_Page/index.tsx
@@ -1,11 +1,23 @@
-import WelcomePage from "./WelcomePage";
-import { Routes, Route } from "react-router-dom";
+import Welcome from "./Welcome";
+import Main from "./Main";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import PAGE_URI from "../4_Shared/constant/pageUri";
 
 const Page = () => {
+  const [cookies] = useCookies(["accessToken"]);
+  const hasAccessToken = !!cookies.accessToken; // accessToken 존재 여부 확인
   return (
-    <div className=" w-full flex justify-center items-center">
+    <div className="w-full flex justify-center items-center">
       <Routes>
-        <Route path="/" element={<WelcomePage />} />
+        <Route path="/" element={hasAccessToken ? <Navigate to={PAGE_URI.MAIN} /> : <Welcome />} />
+        <Route path={PAGE_URI.MAIN} element={<Main />} />
+        <Route path={PAGE_URI.TEAMLIST} element={<Main />} />
+        <Route path={`${PAGE_URI.TEAM}/:teamIdx`} element={<Main />} />
+        <Route path={`${PAGE_URI.COMMUNITY}/:communityIdx`} element={<Main />} />
+        <Route path={PAGE_URI.RANKING} element={<Main />} />
+        <Route path={`${PAGE_URI.PROFILE}/:userIdx`} element={<Main />} />
+        <Route path={PAGE_URI.FREEMATCH} element={<Main />} />
       </Routes>
     </div>
   );
