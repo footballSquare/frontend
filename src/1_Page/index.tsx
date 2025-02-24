3da79a7c7862cb98c70eec34f527373c93912537
RyuDongHo <thffhahs03@gmail.com>
2025-02-24 17:03:06 +0900

    chore: set page background color

--- src/1_Page/index.tsx
+++ src/1_Page/index.tsx
@@ -3,12 +3,13 @@ import Main from "./Main";
 import { Routes, Route, Navigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import PAGE_URI from "../4_Shared/constant/pageUri";
+import FreeMatch from "./FreeMatch";
 
 const Page = () => {
   const [cookies] = useCookies(["accessToken"]);
   const hasAccessToken = !!cookies.accessToken; // accessToken 존재 여부 확인
   return (
-    <div className="w-full flex justify-center items-center">
+    <div className="w-full h-full flex justify-center bg-light-blue">
       <Routes>
         <Route path="/" element={hasAccessToken ? <Navigate to={PAGE_URI.MAIN} /> : <Welcome />} />
         <Route path={PAGE_URI.MAIN} element={<Main />} />
@@ -17,7 +18,7 @@ const Page = () => {
         <Route path={`${PAGE_URI.COMMUNITY}/:communityIdx`} element={<Main />} />
         <Route path={PAGE_URI.RANKING} element={<Main />} />
         <Route path={`${PAGE_URI.PROFILE}/:userIdx`} element={<Main />} />
-        <Route path={PAGE_URI.FREEMATCH} element={<Main />} />
+        <Route path={PAGE_URI.FREEMATCH} element={<FreeMatch />} />
       </Routes>
     </div>
   );
