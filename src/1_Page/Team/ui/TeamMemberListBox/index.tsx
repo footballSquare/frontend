02fffbb8d0e52b686491e0e16b34513c09d74c39
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-02-25 19:45:48 +0900

    feat : 팀원 관리 모달 구현

--- src/1_Page/Team/ui/TeamMemberListBox/index.tsx
+++ src/1_Page/Team/ui/TeamMemberListBox/index.tsx
@@ -1,18 +1,13 @@
 import useGetTeamMembers from "../../../../3_Entity/Team/useGetTeamMembers";
+import MemberCard from "./ui/MemberCard";
 
 const TeamMemberListBox = () => {
   const [teamMember] = useGetTeamMembers();
+
   return (
     <div className=" h-[200px] overflow-scroll">
-      {teamMember.map((member, index) => (
-        <div key={index} className="flex items-center space-x-2">
-          <img
-            src={member.player_list_profile_img}
-            alt={member.player_list_nickname}
-            className="w-8 h-8 rounded-full"
-          />
-          <span className="text-xs">{member.player_list_nickname}</span>
-        </div>
+      {teamMember.map((elem, index) => (
+        <MemberCard {...elem} index={index} />
       ))}
     </div>
   );
---
+++ src/1_Page/Team/ui/TeamMemberListBox/ui/MemberCard/type.tsx
@@ -0,0 +1,5 @@
+import { TeamMembers } from "../../../../../../3_Entity/Team/type";
+
+export type MemberProps = TeamMembers & {
+  index: number;
+};
