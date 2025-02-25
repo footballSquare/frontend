2922dc36f882777da681603edba58c73c3db035a
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-02-25 18:15:23 +0900

    refactor : 리렌더링 최적화

---
+++ src/1_Page/Team/ui/PresentMatchBox/index.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import useGetTeamMatchList from "../../../../3_Entity/Match/useGetTeamMatchList";
+import useInfiniteScrollPaging from "../../../../4_Shared/model/useInfiniteScrollPaging";
+import MatchCard from "./ui/MatchCrad";
+
+const PresentMatchBox = () => {
+  const [page, setPage] = useState(1);
+  const [teamMatchList, hasMoreContent, loading] = useGetTeamMatchList(page);
+  const [observeRef] = useInfiniteScrollPaging(
+    setPage,
+    loading,
+    hasMoreContent
+  );
+
+  return (
+    <div className="flex flex-col space-y-2 h-[300px] overflow-scroll">
+      {teamMatchList.length === 0 ? (
+        <div>진행중인 경기가 없습니다.</div>
+      ) : (
+        teamMatchList.map((elem, index) => (
+          <MatchCard
+            {...elem}
+            index={index}
+            observeRef={
+              teamMatchList.length === index + 1 ? observeRef : undefined
+            }
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default PresentMatchBox;
