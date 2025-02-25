40d3875e422efd2766e42ea9c32127b52031a9d7
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-02-26 00:25:24 +0900

    refactor : css 및 ? 불필요 부분 삭제?

--- src/1_Page/Team/ui/AwardList/ui/HoverTrophy/index.tsx
+++ src/1_Page/Team/ui/AwardList/ui/HoverTrophy/index.tsx
@@ -17,30 +17,22 @@ const Trophy = ({
       <div className="flex items-center justify-center w-full h-full">
         <img
           src={trophyData.championship_list_throphy_img}
-          alt="Trophy"
           className="w-[50px] h-[50px]"
         />
       </div>
-
       {/* Hover 시 보여줄 정보 (200x200 고정 크기) */}
+      {/* 인덱스에 따라서 잘리는 위치 조정 */}
       <div
         className={`absolute ${
           index === 0 ? "left-0" : index === length - 1 ? "right-0" : "left-1/2"
-        } top-1/2 bg-white rounded-lg flex flex-col items-center justify-center w-[200px] h-[200px] transition-all duration-300 ease-in-out transform ${
-          index === 0
-            ? "translate-x-0"
-            : index === length - 1
-            ? "translate-x-0"
-            : "-translate-x-1/2"
-        } -translate-y-1/2 z-50 shadow-lg p-4 border border-gray-300 scale-0 group-hover:scale-100`}
-        style={{
-          whiteSpace: "nowrap",
-          overflow: "visible",
-          pointerEvents: "auto",
-        }}>
+        } 
+              top-1/2 bg-white rounded-lg flex flex-col items-center justify-center 
+              w-[200px] h-[200px] transition-transform duration-300 transform 
+              ${index !== 0 && index !== length - 1 ? "-translate-x-1/2" : ""} 
+              -translate-y-1/2 z-50 shadow-lg p-4 border border-gray-300 
+              scale-0 group-hover:scale-100 whitespace-nowrap overflow-visible pointer-events-auto`}>
         <img
           src={trophyData.championship_list_throphy_img}
-          alt="Trophy"
           className="w-[80px] h-[80px] mb-2"
         />
         <h3 className="text-lg font-semibold text-center">
