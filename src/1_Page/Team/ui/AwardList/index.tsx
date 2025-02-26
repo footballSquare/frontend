662fae14872006bc82f95dfc3344687e233d3eef
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-02-27 02:39:35 +0900

    feat : 트로피 개수 동적 계산후 인디케이터 개수 지정

--- src/1_Page/Team/ui/AwardList/index.tsx
+++ src/1_Page/Team/ui/AwardList/index.tsx
@@ -3,40 +3,35 @@ import useGetTeamAwards from "../../../../3_Entity/Team/useGetTeamAwards";
 import React from "react";
 import useInfiniteScrollPaging from "../../../../4_Shared/model/useInfiniteScrollPaging";
 import useIndexAutoAnimate from "./model/useIndexAutoAnimate";
+import useIndicator from "./model/useIndicator";
 
 const trophiesPerGroup = 5;
 
 const AwardList = () => {
   const [page, setPage] = React.useState<number>(1);
   const [teamAwards, hasMoreContent, loading] = useGetTeamAwards(page);
-  const totalGroups = Math.ceil(teamAwards.length / trophiesPerGroup);
-
   const [currentIndex, handleClickCurrentIndex] = useIndexAutoAnimate(
     teamAwards.length
   );
-
   const [observeRef] = useInfiniteScrollPaging(
     setPage,
     loading,
     hasMoreContent
   );
 
+  const containerRef = React.useRef<HTMLDivElement>(null);
+  const [visibleTrophyCount] = useIndicator(containerRef);
+
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="w-[100%] flex justify-center items-center">
-        <div className="flex gap-4 mt-4 w-[100%]">
+        <div className="flex gap-4 mt-4 w-[100%]" ref={containerRef}>
           {/* 트로피 컨테이너 */}
-          <div className="overflow-hidden relative w-full h-[50px]">
+          <div className="overflow-hidden relative w-full h-[80px] p-4">
             {teamAwards.map((trophyData, index) => (
               <div
                 key={index}
-                className={`absolute transition-all duration-700 ease-in-out ${
-                  index === currentIndex
-                    ? "scale-110 opacity-100"
-                    : index > currentIndex
-                    ? "opacity-70"
-                    : "opacity-50"
-                }`}
+                className={`absolute transition-all duration-700 ease-in-out `}
                 ref={teamAwards.length - 1 === index ? observeRef : null}
                 style={{
                   transform: `translateX(${(index - currentIndex) * 120}px)`,
@@ -49,7 +44,7 @@ const AwardList = () => {
       </div>
       {/* 현재 트로피 위치 인디케이터 */}
       <div className="flex gap-2 mt-4">
-        {Array.from({ length: totalGroups }).map((_, index) => (
+        {Array.from({ length: visibleTrophyCount }).map((_, index) => (
           <div
             key={`indicator-${index}`}
             className={`w-2 h-2 rounded-full ${
