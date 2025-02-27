cbb99ea719be2e5b6c87e57dcec8595a58fbdd77
RyuDongHo <thffhahs03@gmail.com>
2025-02-27 23:17:59 +0900

    chore: add constant & mockdata api 완성 전에 사용할 임시 데이터와, 상수 추가

---
+++ src/4_Shared/mock/matchWaitList.ts
@@ -0,0 +1,23 @@
+export const mockMatchWaitList = {
+  match_waitlist: {
+    "0": [
+      {
+        player_list_idx: 60,
+        player_list_nickname: "카시야스",
+        player_list_url: "https://~~",
+      },
+      {
+        player_list_idx: 63,
+        player_list_nickname: "퐈이야",
+        player_list_url: "https://~~",
+      },
+    ],
+    "1": [
+      {
+        player_list_idx: 13,
+        player_list_nickname: "이영표",
+        player_list_url: "https://~~",
+      },
+    ],
+  },
+};
