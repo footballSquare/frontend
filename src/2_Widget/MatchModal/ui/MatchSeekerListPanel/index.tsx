4d14d847b2f7d3994f6662ef00435c0865f2696f
RyuDongHo <thffhahs03@gmail.com>
2025-02-27 23:20:21 +0900

    feat: MatchModal MatchModal ui 재설계

--- src/2_Widget/MatchModal/ui/MatchSeekerListPanel/index.tsx
+++ src/2_Widget/MatchModal/ui/MatchSeekerListPanel/index.tsx
@@ -2,31 +2,31 @@ import React from "react";
 import WaitingPlayerList from "../../../WaitingPlayerList";
 import up_icon from "../../../../4_Shared/assets/svg/up.svg";
 import down_icon from "../../../../4_Shared/assets/svg/down.svg";
-const WaitingPlayerListPanel = () => {
-  const [isWaitingPlayerListOpen, setIsWaitingPlayerListOpen] =
+const MatchSeekerListPanel = () => {
+  const [isMatchSeekerListPanel, setIsMatchSeekerListPanel] =
     React.useState(false);
   return (
-    <div className=" absolute top-12 right-6 flex flex-col h-[calc(100%_-_96px)] w-[30%]">
+    <div className=" absolute top-10 right-12 flex flex-col h-[calc(100%_-_96px)] w-[30%]">
       <div className=" flex justify-between items-center">
         <h4 className=" font-bold text-blue">매치 참여 희망 인원 현황</h4>
-        {isWaitingPlayerListOpen ? (
+        {isMatchSeekerListPanel ? (
           <button
-            onClick={() => setIsWaitingPlayerListOpen(!isWaitingPlayerListOpen)}
+            onClick={() => setIsMatchSeekerListPanel(!isMatchSeekerListPanel)}
           >
             <img src={up_icon} alt="up" />
           </button>
         ) : (
           <button
-            onClick={() => setIsWaitingPlayerListOpen(!isWaitingPlayerListOpen)}
+            onClick={() => setIsMatchSeekerListPanel(!isMatchSeekerListPanel)}
           >
             <img src={down_icon} alt="down" />
           </button>
         )}
       </div>
 
-      {isWaitingPlayerListOpen && <WaitingPlayerList />}
+      {isMatchSeekerListPanel && <WaitingPlayerList />}
     </div>
   );
 };
 
-export default WaitingPlayerListPanel;
+export default MatchSeekerListPanel;
---
+++ src/2_Widget/MatchModal/ui/WaitingList/index.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { matchPosition } from "../../../../4_Shared/constant/matchPosition";
+import { WaitingListProps } from "./type";
+import applyBtn from "../../../../4_Shared/assets/svg/applyBtn.svg";
+
+const WaitingList = (props: WaitingListProps) => {
+  const [selectedPostion, setSelectedPosition] = React.useState<number>(0);
+  const { match_formation_position, matchParticipants, matchWaitList } = props;
+  
+
+  return (
+    <div>
+      <select
+        className=" w-[164px] h-[32px] rounded-[4px] text-center border-1 border-blue"
+        onChange={(e) => setSelectedPosition(Number(e.target.value))}
+      >
+        {match_formation_position.map((position) => (
+          <option key={position} value={position}>
+            {matchPosition[position]}
+          </option>
+        ))}
+      </select>
+
+      {matchWaitList && (
+        <div className="flex flex-col gap-2">
+          {matchWaitList[selectedPostion]?.map((player) => (
+            <div
+              key={player.player_list_idx}
+              className="flex gap-2 justify-around "
+            >
+              {/* <img src={player.player_list_url} alt="player" /> */}
+              <p>{player.player_list_nickname}</p>
+              <button>
+                <img src={applyBtn} alt="" />
+              </button>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default WaitingList;
