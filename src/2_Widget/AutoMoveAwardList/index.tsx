fc36113d2e25905b8e4fe7da84bd784554f5f2f4
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-02-27 18:57:20 +0900

    refactor : autoMoveAwardList 위젯 분리

--- src/2_Widget/AutoMoveAwardList/index.tsx
+++ src/2_Widget/AutoMoveAwardList/index.tsx
@@ -1,25 +1,25 @@
 import HoverTrophy from "./ui/HoverTrophy";
-import useGetTeamAwards from "../../../../3_Entity/Team/useGetTeamAwards";
 import React from "react";
 import useIndexAutoAnimate from "./model/useIndexAutoAnimate";
 import useIndicator from "./model/useIndicator";
+import { TeamAwards } from "../../3_Entity/Team/type";
 
-const AwardList = () => {
-  const [teamAwards] = useGetTeamAwards();
+const AutoMoveAwardList = (props: { awards: TeamAwards[] }) => {
+  const { awards } = props;
   const [currentIndex, handleClickCurrentIndex] = useIndexAutoAnimate(
-    teamAwards.length
+    awards.length
   );
 
   const containerRef = React.useRef<HTMLDivElement>(null);
   const [visibleTrophyCount] = useIndicator(containerRef); // 현재 컨테이너 사이즈에 따른 트로피 개수
 
   return (
-    <div className="flex flex-col justify-center items-center">
-      <div className="w-[100%] flex justify-center items-center">
+    <div>
+      <div className="flex justify-center items-center">
         <div className="flex gap-4 mt-4 w-[100%]" ref={containerRef}>
           {/* 트로피 컨테이너 */}
           <div className="overflow-hidden relative w-full h-[80px] p-4">
-            {teamAwards.map((trophyData, index) => (
+            {awards.map((trophyData, index) => (
               <div
                 key={index}
                 className={`absolute transition-all duration-700 ease-in-out `}
@@ -35,7 +35,7 @@ const AwardList = () => {
       {/* 현재 트로피 위치 인디케이터 */}
       <div className="flex gap-2 mt-4">
         {/* 전체 트로피 개수 / 현재 보이는 트로피 개수 인디케이터 개수 계산*/}
-        {Array.from({ length: teamAwards.length / visibleTrophyCount }).map(
+        {Array.from({ length: awards.length / visibleTrophyCount }).map(
           (_, index) => (
             <div
               key={`indicator-${index}`}
@@ -56,4 +56,4 @@ const AwardList = () => {
   );
 };
 
-export default AwardList;
+export default AutoMoveAwardList;
