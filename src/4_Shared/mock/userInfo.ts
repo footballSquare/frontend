7daf958b592c282a9367f83240ac7d9d89591a5a
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-03-01 01:21:59 +0900

    fix : 대쉬보드 발리데이트 수정

--- src/4_Shared/mock/userInfo.ts
+++ src/4_Shared/mock/userInfo.ts
@@ -6,10 +6,10 @@ export const mockUserInfo = {
     platform: 1,
     team: "Champion FC",
     position: "Striker",
-    tag_discord: "#Ace1234",
+    tag_discord: "#1234",
     tag: "#000000",
     mmr: 2500,
-    phone_number: "KR 010-1234-5678",
+    phone_number: "010-1234-5678",
     match_count: 20,
     winning_rate: 12,
     trophies: [
