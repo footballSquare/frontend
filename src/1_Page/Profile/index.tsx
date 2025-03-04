6b6e689348ad457215fa595c305afccb80cfd18f
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-03-04 19:22:04 +0900

    refactor : style className으로 수정'

--- src/1_Page/Profile/index.tsx
+++ src/1_Page/Profile/index.tsx
@@ -1,5 +1,3 @@
-import STYLE from "./type";
-
 import useGetMyInfo from "../../3_Entity/Account/useGetUserInfo";
 import AwardDashBoard from "./ui/AwardDashBoard/indes";
 import PlayerDashBoard from "./ui/PlayerDashBoard";
@@ -12,14 +10,14 @@ const Profile = () => {
   const awardProps = { winning_rate, trophies, match_count };
 
   return (
-    <main className={STYLE.main}>
+    <main className="flex flex-wrap gap-4 w-full justify-center bg-white shadow-md rounded-lg p-4">
       {/* Player Dashboard */}
-      <div className={STYLE.playerDashboard}>
+      <div className="w-[90%] sm:w-[55%] min-w-[300px]">
         <PlayerDashBoard userInfo={{ ...playerProps }} />
       </div>
 
       {/* Award Dashboard */}
-      <div className={STYLE.awardDashboard}>
+      <div className="w-[90%] sm:w-[40%] min-w-[250px] bg-white shadow-md rounded-lg p-4">
         <AwardDashBoard awardInfo={awardProps} />
       </div>
     </main>
