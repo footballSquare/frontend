e394ebdcca6cf9e3179a0a8eadaea130be53e845
RyuDongHo <thffhahs03@gmail.com>
2025-03-09 19:30:28 +0900

    feat: useGetCommunityInfo Entity Entity Layer에 useGetCommunityInfo hook 추가

---
+++ src/3_Entity/Community/type.ts
@@ -0,0 +1,7 @@
+export type CommunityInfo = {
+  community_list_idx: number;
+  community_list_name: string;
+  community_list_notice: string;
+  community_list_banner: string;
+  community_list_emblem: string;
+};
---
+++ src/4_Shared/mock/communityInfo.ts
@@ -0,0 +1,7 @@
+export const mockCommunityInfo = {
+  community_list_idx: 0,
+  community_list_name: "KFPL",
+  community_list_notice: "현재 운영진 모집 중에 있습니다.",
+  community_list_banner: "URL",
+  community_list_emblem: "URL",
+};
