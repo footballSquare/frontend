810d44e425d8de4587d151bcb4ba370d0687b406
RyuDongHo <thffhahs03@gmail.com>
2025-03-10 23:07:02 +0900

    feat: CommunityPage ui Community page의 ui 초기 세팅 추가

---
+++ src/1_Page/Community/ui/CommunityPostList/index.tsx
@@ -0,0 +1,5 @@
+const CommunityPostList = () => {
+  return <div>커뮤니티 게시글</div>;
+};
+
+export default CommunityPostList;
---
+++ src/1_Page/Community/ui/CommunityTeamList/index.tsx
@@ -0,0 +1,5 @@
+const CommunityTeamList = () => {
+  return <div>커뮤니티 소속 팀 목록</div>
+};
+
+export default CommunityTeamList;
