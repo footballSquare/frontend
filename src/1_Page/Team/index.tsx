c04a100bd8b43eb202f9bf704775267b3e7ae71a
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-03-15 18:44:05 +0900

    refactor : 일부 페이지 시맨틱 설정

--- src/1_Page/Team/index.tsx
+++ src/1_Page/Team/index.tsx
@@ -27,19 +27,19 @@ const Team = () => {
       ) : (
         <div>
           {/* 배너 */}
-          <div className="flex justify-center">
+          <section className="flex justify-center">
             <img
               className="w-full h-[200px] object-cover rounded-lg"
               src={teamInfo?.team_list_banner}
               alt="팀 배너"
             />
-          </div>
+          </section>
 
           {/* 트로피 */}
           <TeamAwards />
 
           {/* 내용 */}
-          <div className="flex flex-col gap-5 sm:grid grid-cols-5 w-full ">
+          <article className="flex flex-col gap-5 sm:grid grid-cols-5 w-full ">
             <div className="w-full flex flex-wrap gap-1 sm:col-span-2">
               <div className="flex flex-col items-center">
                 <div className="flex items-center ">
@@ -48,7 +48,7 @@ const Team = () => {
                     alt="Team Emblem"
                     className="w-16 h-16 rounded-full object-cover border border-gray-200"
                   />
-                  <div className="flex flex-col items-center min-w-[120px]">
+                  <section className="flex flex-col items-center min-w-[120px]">
                     <h1
                       style={{
                         fontSize: "1.5rem",
@@ -65,15 +65,15 @@ const Team = () => {
                       isTeamReader={isTeamReader}
                       teamListIdx={teamInfo.team_list_idx}
                     />
-                  </div>
+                  </section>
                 </div>
-                <div className="flex flex-col items-start w-full">
+                <section className="flex flex-col items-start w-full">
                   <h2 className="text-base font-semibold">팀 연혁</h2>
                   <HistoryListBox />
-                </div>
+                </section>
               </div>
 
-              <div className="space-y-3">
+              <section className="space-y-3">
                 <div className="max-w-[200px]">
                   <h2 className="text-base font-semibold">팀 설명</h2>
                   <p className="text-gray-600 text-xs whitespace-pre-line">
@@ -84,14 +84,14 @@ const Team = () => {
                     <TeamMemberListBox />
                   </div>
                 </div>
-              </div>
+              </section>
             </div>
 
             <div className="space-y-3 sm:col-span-3">
               <h2 className="text-base font-semibold">현재 경기</h2>
               <PresentMatchBox team_list_idx={teamInfo.team_list_idx} />
             </div>
-          </div>
+          </article>
         </div>
       )}
     </main>
