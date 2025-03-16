f6299981f2dc54718403124e01b0aead160af16f
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-03-16 21:19:17 +0900

    fix : schema 수정

--- src/2_Widget/PlayerCard/lib/schema.ts
+++ src/2_Widget/PlayerCard/lib/schema.ts
@@ -1,22 +1,22 @@
 import * as yup from "yup";
 
 export const schema = yup.object().shape({
-  profile_img: yup
+  img: yup
     .mixed<File>()
-    .required("프로필 이미지는 필수 입력 항목입니다.")
+    .nullable()
+    .default(null)
     .test("fileType", "JPG, PNG, SVG 형식만 가능합니다.", (value) => {
-      if (!value || (value instanceof FileList && value.length === 0)) {
-        return true;
+      if (value && value instanceof File) {
+        return isFileExtension(value);
       }
-
-      const allowedMimeTypes = ["image/jpeg", "image/png", "image/svg+xml"];
-      const allowedExtensions = [".jpg", ".jpeg", ".png", ".svg"];
-
-      const isValidMimeType = allowedMimeTypes.includes(value.type);
-      const isValidExtension = allowedExtensions.some((ext) =>
-        value.name.toLowerCase().endsWith(ext)
-      );
-
-      return isValidMimeType || isValidExtension;
+      return false;
+    })
+    .test("fileType", "2MB를 초과할 수 없습니다.", (value) => {
+      if (value && value instanceof File) {
+        if (value.size >= 2 * 1024 * 1024) {
+          return false;
+        }
+      }
+      return true;
     }),
 });
