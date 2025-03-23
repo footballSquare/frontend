b74f7b66fcdc17d276514e8846b71d1b025e4644
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-03-23 18:12:23 +0900

    refactor : enum으로 분리

--- src/1_Page/Championship/ui/DashBoard/constant/activeTab.ts
+++ src/1_Page/Championship/ui/DashBoard/constant/activeTab.ts
@@ -1,5 +1,5 @@
-export const ACTIVE_TAB = {
-  PLAYERS: "players",
-  TEAM: "team",
-  MATCH: "match",
-};
+export enum ACTIVE_TAB {
+  PLAYERS = "players",
+  TEAM = "team",
+  MATCH = "match",
+}
--- src/1_Page/Championship/ui/DashBoard/index.tsx
+++ src/1_Page/Championship/ui/DashBoard/index.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { navList } from "./constant/navList";
+import { ACTIVE_TAB } from "./constant/activeTab";
 
 import ParticipationMembers from "./ui/ParticipationMembers";
 import TeamAndMatchContainerProps from "./ui/TeamAndMatchContainerProps";
-import { ACTIVE_TAB } from "./constant/activeTab";
 
 const DashBoard = (props: DashBoardProps) => {
   const { championshipIdx, isLeague } = props;
-  const [activeTab, setActiveTab] = React.useState<string>(ACTIVE_TAB.PLAYERS);
+  const [activeTab, setActiveTab] = React.useState<ACTIVE_TAB>(
+    ACTIVE_TAB.PLAYERS
+  );
 
   return (
     <div className="w-full p-4">
@@ -15,7 +17,7 @@ const DashBoard = (props: DashBoardProps) => {
         {navList.map(({ id, label }) => (
           <button
             key={id}
-            onClick={() => setActiveTab(id)}
+            onClick={() => setActiveTab(id as ACTIVE_TAB)}
             className={`px-4 py-2 rounded-md transition ${
               activeTab === id ? "bg-blue-800" : "bg-blue-600 hover:bg-blue-700"
             }`}>
