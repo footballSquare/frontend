916046b603ede3522cbc8135379a2ed0ac73743d
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-03-31 00:58:08 +0900

    fix : put 엔티티 수정

--- src/3_Entity/Championship/types/request.d.ts
+++ src/3_Entity/Championship/types/request.d.ts
@@ -3,3 +3,13 @@ type UsePostCreateChampionshipMatchProps = {
   second_team_idx: number;
   match_match_start_time: string;
 };
+
+type UsePutChampionshipEndProps = {
+  championship_list_name: number;
+  championship_list_description: number;
+  championship_list_throphy_img: string;
+  championship_list_start_date: string;
+  championship_list_end_date: string;
+  championship_list_color: string;
+  participation_team: number[];
+};
