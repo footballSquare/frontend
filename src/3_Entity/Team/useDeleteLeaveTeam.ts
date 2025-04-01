fd607291e9be6f3326488ae1411662f3c5464abf
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-04-01 15:11:45 +0900

    refacotr : team - delete api 엔드포인트 연결

--- src/3_Entity/Team/useDeleteLeaveTeam.ts
+++ src/3_Entity/Team/useDeleteLeaveTeam.ts
@@ -1,14 +1,14 @@
 import React from "react";
-import { useFetch } from "../../4_Shared/util/apiUtil";
+import { useFetchData } from "../../4_Shared/util/apiUtil";
 
 const useDeleteLeaveTeam = (
   teamListIdx: number
 ): [deleteLeaveTeam: () => void, serverState: unknown, loading: boolean] => {
-  const [serverState, request, loading] = useFetch();
+  const [serverState, request, loading] = useFetchData();
 
   const deleteLeaveTeam = () => {
-    request({ teamListIdx });
-    console.log("팀 탈퇴 실행", teamListIdx);
+    const endPoint = `/team/${teamListIdx}/leave`;
+    request("DELETE", endPoint, null, true);
   };
 
   React.useEffect(() => {
