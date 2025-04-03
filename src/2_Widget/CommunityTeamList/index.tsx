aaeed7ce33b38a948b23c90a02dff49ad6563cb9
RyuDongHo <thffhahs03@gmail.com>
2025-04-04 02:57:19 +0900

    chore:communityTeamList ui design community team list ui의 디자인 수정

--- src/2_Widget/CommunityTeamList/index.tsx
+++ src/2_Widget/CommunityTeamList/index.tsx
@@ -2,7 +2,7 @@ import useGetCommunityTeamList from "../../3_Entity/Community/useGetCommunityTea
 import React from "react";
 import useInfiniteScrollPaging from "../../4_Shared/model/useInfiniteScrollPaging";
 const CommunityTeamList = (props: CommunityTeamListProps) => {
-  const { communityIdx } = props;
+  const { communityIdx, modifyMode } = props;
   const [page, setPage] = React.useState<number>(0);
   const [communityTeamList, hasMoreContent, loading] = useGetCommunityTeamList({
     communityIdx,
@@ -14,17 +14,27 @@ const CommunityTeamList = (props: CommunityTeamListProps) => {
     hasMoreContent
   );
   return (
-    <div className="bg-white rounded-lg shadow w-full flex flex-col gap-4 overflow-y-auto">
+    <div className="bg-gray-50 rounded-xl shadow-md w-full flex flex-col gap-4 overflow-y-auto p-4">
       {communityTeamList.map((team, index) => {
         return (
           <div
             key={index}
-            className={` border border-gray shadow-lg rounded-lg p-2 min-h-[120px]`}
+            className={`border flex flex-col gap-4 justify-between border-gray-300 shadow-md rounded-lg p-4 min-h-[120px] hover:bg-blue-100 transition-all duration-300`}
             ref={
               communityTeamList.length === index + 1 ? observeRef : undefined
             }
           >
-            <h3>TEAM - {team.team_list_name}</h3>
+            <h3 className="text-lg font-semibold text-gray-800">
+              TEAM - {team.team_list_name}
+            </h3>
+            <button className="border bg-white border-gray-300 p-2 rounded-lg text-sm font-medium text-gray-700 hover:bg-gray-100 transition">
+              팀 페이지로 이동하기
+            </button>
+            {modifyMode && (
+              <button className="bg-red-500 p-2 rounded-lg text-white text-sm font-medium hover:bg-red-600 transition">
+                팀 방출하기
+              </button>
+            )}
           </div>
         );
       })}
