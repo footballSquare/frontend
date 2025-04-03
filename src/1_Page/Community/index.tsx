9e18133895f5b6ce6014b38075caede875bbb4b0
RyuDongHo <thffhahs03@gmail.com>
2025-04-04 02:56:38 +0900

    feat: Communit staff ui & state community staff인 경우만 보이는 ui와 community staff 유무를 확인하기 위한 전역 상태 관리를 추가

--- src/1_Page/Community/index.tsx
+++ src/1_Page/Community/index.tsx
@@ -4,101 +4,119 @@ import CommunityStaffList from "./ui/CommunityStaffList";
 import CommunityTeamList from "../../2_Widget/CommunityTeamList";
 import useModifyMode from "./model/useModifyMode";
 import useGetCommunityInfo from "../../3_Entity/Community/useGetCommunityInfo";
+import useIsCommunityStaffStore from "../../4_Shared/zustand/useIsCommunityStaffStore";
+
 const Community = () => {
   const { communityIdx } = useParams();
-  const isCommunityAdmin = true;
   const [modifyMode, toggleModifyMode] = useModifyMode();
   const [communityInfo] = useGetCommunityInfo({
     communityIdx: Number(communityIdx),
   });
+  const { isCommunityStaff } = useIsCommunityStaffStore();
   return (
-    <div className="bg-white w-full p-4 flex gap-4">
+    <div className="h-full w-full p-6 flex gap-6">
       {/* Left Sidebar */}
-      <div className="flex flex-col gap-4 bg-white rounded-lg shadow p-4 w-full max-w-[360px]">
+      <div className="flex flex-col gap-6 bg-white rounded-xl shadow-md p-6 w-full max-w-[360px]">
         {/* 커뮤니티 앰블럼, 커뮤니티 명 */}
         <div className="flex flex-col items-center">
           <img
             src={communityInfo.community_list_emblem}
             alt="emblem"
-            className="w-16 h-16 rounded-full bg-blue-500 flex items-center justify-center text-white text-2xl mb-2"
+            className="w-20 h-20 rounded-full bg-blue-500 flex items-center justify-center text-white text-2xl mb-4 shadow-lg"
           />
-          <h2 className="text-xl font-bold">
+          <h2 className="text-2xl font-semibold text-gray-800">
             {communityInfo.community_list_name}
           </h2>
         </div>
         {/* 배너&엠블럼 수정 버튼 */}
         {modifyMode && (
-          <div className="flex flex-col gap-2 mx-auto">
+          <div className="flex flex-col gap-4 mx-auto">
             <div className="flex flex-col items-center">
-              <label className="block text-sm font-medium text-blue mb-1">
+              <label className="block text-sm font-medium text-gray-700 mb-2">
                 배너 이미지 변경
               </label>
               <input
                 type="file"
                 accept="image/*"
-                className="block w-full text-sm border border-gray rounded-lg cursor-pointer focus:outline-none"
+                className="block w-full text-sm border border-gray-300 rounded-lg cursor-pointer focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             </div>
             <div className="flex flex-col items-center">
-              <label className="block text-sm font-medium text-blue mb-1">
+              <label className="block text-sm font-medium text-gray-700 mb-2">
                 엠블럼 이미지 변경
               </label>
               <input
                 type="file"
                 accept="image/*"
-                className="block w-full text-sm text-gray-900 border border-gray rounded-lg cursor-pointer focus:outline-none"
+                className="block w-full text-sm border border-gray-300 rounded-lg cursor-pointer focus:outline-none focus:ring-2 focus:ring-blue-500"
               />
             </div>
           </div>
         )}
 
-        {/* 커뮤니티 운영진 목록 */}
+        {/* 커뮤니티 운영진 목록 & 운영진 지원 */}
         <CommunityStaffList communityIdx={Number(communityIdx)} />
 
-        {/* 커뮤니티 상호작용 버튼 (운영진 지원, 수정) */}
-        {isCommunityAdmin ? (
+        {/* 커뮤니티 수정 버튼 */}
+        {isCommunityStaff && (
           <button
-            className=" p-2 border border-gray shadow-lg rounded-[4px] text-sm"
+            className="p-3 border border-gray-300 shadow-md rounded-lg text-sm bg-blue-500 text-white hover:bg-blue-600 transition"
             onClick={toggleModifyMode}
           >
             {modifyMode ? "수정 취소" : "커뮤니티 정보 수정하기"}
           </button>
-        ) : (
-          <button className=" p-2 border border-gray shadow-lg rounded-[4px] text-sm">
-            커뮤니티 운영진 지원
-          </button>
         )}
       </div>
 
-      <div className=" flex flex-col gap-4 w-full">
+      <div className="flex flex-col gap-6 w-full">
         {/* 배너 */}
-        <div className="w-full min-h-[160px] bg-blue-600 flex items-center justify-center text-white text-xl">
+        <div className="w-full min-h-[160px] bg-blue-500 flex items-center justify-center text-white text-2xl font-semibold rounded-lg shadow-md">
           배너 이미지
         </div>
 
-        {modifyMode ? (
-          <div></div>
-        ) : (
-          <div className=" flex gap-4">
-            {/* ChampionshipList */}
-            <div className="w-full">
-              <h3 className="font-bold mb-2">Championship</h3>
-              <ChampionshipList communityIdx={Number(communityIdx)} />
-            </div>
+        <div className="flex gap-6 overflow-auto max-h-[80%]">
+          {modifyMode ? (
+            <>
+              {/* CommunityTeamList */}
+              <div className="w-full">
+                <h3 className="font-semibold text-lg text-gray-800 mb-4">
+                  Community Teams
+                </h3>
+                <CommunityTeamList
+                  communityIdx={Number(communityIdx)}
+                  modifyMode={modifyMode}
+                />
+              </div>
 
-            {/* BoardList */}
-            {/* <div className="w-full">
-            <h3 className="font-bold mb-2">Community Board</h3>
-            <BoardList category={0} />
-          </div> */}
+              {/* 가입 신청 팀 목록 */}
 
-            {/* CommunityTeamList */}
-            <div className="w-full">
-              <h3 className="font-bold mb-2">Community Teams</h3>
-              <CommunityTeamList communityIdx={Number(communityIdx)} />
-            </div>
-          </div>
-        )}
+              {/* 운영진 가입 신청 유저 목록 */}
+            </>
+          ) : (
+            <>
+              {/* ChampionshipList */}
+              <div className="w-full">
+                <h3 className="font-semibold text-lg text-gray-800 mb-4">
+                  Championship
+                </h3>
+                <ChampionshipList communityIdx={Number(communityIdx)} />
+              </div>
+
+              {/* BoardList */}
+
+              {/* CommunityTeamList */}
+              <div className="w-full">
+                <h3 className="font-semibold text-lg text-gray-800 mb-4">
+                  Community Teams
+                </h3>
+                <CommunityTeamList
+                  communityIdx={Number(communityIdx)}
+                  modifyMode={modifyMode}
+                />
+              </div>
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
