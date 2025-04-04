723f8230f69a98add3c00ae1dbda811df1a30281
RyuDongHo <thffhahs03@gmail.com>
2025-04-04 16:05:35 +0900

    chore: strict mode strict mode 제거

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { CookiesProvider } from "react-cookie";
 import App from "./0_App";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
       <CookiesProvider>
         <App />
       </CookiesProvider>
-  </StrictMode>
 );
