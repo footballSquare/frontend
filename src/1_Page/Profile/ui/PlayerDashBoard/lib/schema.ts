771fb5db11d0de7f11bdc080ea28a3db34a812a0
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-04-05 16:47:08 +0900

    fix : 프로필 수정 관련 수정

--- src/1_Page/Profile/ui/PlayerDashBoard/lib/schema.ts
+++ src/1_Page/Profile/ui/PlayerDashBoard/lib/schema.ts
@@ -2,41 +2,24 @@ import * as yup from "yup";
 
 // 입력 예외처리 yup
 export const schema = yup.object().shape({
-  state: yup
-    .number()
-    .required("상태메시지는 필수 입력 항목입니다.")
-    .min(2, "상태메시지는 최소 2글자 이상이어야 합니다.")
-    .max(20, "상태메시지는 최대 20글자를 초과할 수 없습니다."),
-
-  message: yup
-    .string()
-    .required("상태메시지는 필수 입력 항목입니다.")
-    .min(2, "상태메시지는 최소 2글자 이상이어야 합니다.")
-    .max(20, "상태메시지는 최대 20글자를 초과할 수 없습니다."),
-
   nickname: yup
     .string()
     .required("닉네임은 필수 입력 항목입니다.")
     .min(2, "닉네임은 최소 2글자 이상이어야 합니다.")
     .max(30, "닉네임은 최대 30글자를 초과할 수 없습니다."),
-
   platform: yup
     .string()
-    .required("플랫폼은 필수 선택 항목입니다.")
-    .min(2, "플랫폼은 최소 2글자 이상이어야 합니다.")
-    .max(20, "플랫폼은 최대 20글자를 초과할 수 없습니다."),
-
-  team: yup
-    .string()
-    .required("팀은 필수선택사항")
-    .min(2, "팀명은 최소 2글자 이상이어야 합니다.")
-    .max(50, "팀명은 최대 50글자를 초과할 수 없습니다."),
-
-  position: yup
-    .string()
-    .required("포지션은 필수선택사항")
-    .min(2, "포지션은 최소 2글자 이상이어야 합니다.")
-    .max(20, "포지션은 최대 20글자를 초과할 수 없습니다."),
-
+    .required("플랫폼은 필수 입력 항목입니다.")
+    .oneOf(
+      ["pc", "xbox", "playstation", "X"] as const,
+      "유효하지 않은 플랫폼입니다."
+    ),
   discord_tag: yup.string().required("디스코드 태그는 필수값입니다."),
+  common_status_idx: yup.number().required("common_status_idx는 필수값입니다."),
+  state_message: yup
+    .string()
+    .required("상태메시지는 필수 입력 항목입니다.")
+    .min(2, "상태메시지는 최소 2글자 이상이어야 합니다.")
+    .max(20, "상태메시지는 최대 20글자를 초과할 수 없습니다."),
+  position: yup.number().required("포지션은 필수값입니다."),
 });
--- src/1_Page/Profile/ui/PlayerDashBoard/util/convert.ts
+++ src/1_Page/Profile/ui/PlayerDashBoard/util/convert.ts
@@ -1,14 +1,7 @@
-import { matchPosition } from "../../../../../4_Shared/constant/matchPosition";
-import { commonStatusIdx } from "../../../../../4_Shared/constant/commonStatusIdx";
-
 export const convertToPostData = (data: UserInfoForm): UsePutUserInfoProps => {
   const postFormData = {
-    nickname: data.nickname,
-    platform: data.platform,
-    state: data.state,
-    message: data.message,
-    discord_tag: data.discord_tag,
-    team_idx: data.team,
+    ...data,
+    platform: data.platform === "X" ? null : data.platform,
   };
 
   return postFormData;
@@ -19,7 +12,6 @@ export const convertToInfoForm = (
 ): UserInfoForm => {
   return {
     ...userInfo,
-    position: matchPosition[Number(userInfo.position)],
-    common_status_idx: commonStatusIdx[Number(userInfo.common_status_idx)],
+    platform: userInfo.platform === null ? "X" : userInfo.platform,
   };
 };
