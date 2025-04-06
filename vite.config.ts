56c71f6d3aa7f9dc80c03753e3ac47cfb952b7ec
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-04-06 22:09:46 +0900

    refactor : 불필요 폴더 삭제

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default ({ mode }: { mode: string }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+export default () => {
   return defineConfig({
     server: {
       proxy: {
         "/api": {
-          target: env.VITE_API_URL,
+          target: "https://api.footballsquare.co.kr",
           changeOrigin: true,
+          secure: false,
           rewrite: (path) => path.replace(/^\/api/, ""),
         },
       },
