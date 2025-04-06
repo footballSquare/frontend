3eb4636cd8802b88f02fe3f7c51cfc10b6a661e0
RyuDongHo <thffhahs03@gmail.com>
2025-04-06 16:19:11 +0900

    fix: build error

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import fs from "fs";
-import path from "path";
 
 export default defineConfig({
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "localhost-key.pem")),
-      cert: fs.readFileSync(path.resolve(__dirname, "localhost.pem")),
-    },
-  },
   plugins: [react(), tailwindcss()],
 });
