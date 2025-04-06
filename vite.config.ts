73984217ca24ca557151a4d731e62edbb50c2108
RyuDongHo <thffhahs03@gmail.com>
2025-04-06 15:58:51 +0900

    docs: gitignore & config gitignore와 config 설정 변경. 로컬에서 https로 테스트하게 수정했습니다

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import fs from "fs";
+import path from "path";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-})
+  server: {
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, "localhost-key.pem")),
+      cert: fs.readFileSync(path.resolve(__dirname, "localhost.pem")),
+    },
+  },
+  plugins: [react(), tailwindcss()],
+});
