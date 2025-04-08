a08c53b5e620ddb8043a0bebe2ec7fc512c59988
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-04-09 01:02:23 +0900

    fix : userInfo type 수정으로 인한 card type 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-<<<<<<< HEAD
-export default () => {
-  return defineConfig({
-    server: {
-      proxy: {
-        "/api": {
-          target: "https://api.footballsquare.co.kr",
-          changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ""),
-        },
-      },
-    },
-    plugins: [react(), tailwindcss()],
-  });
-};
-=======
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 });
->>>>>>> develop
