ad50ba5e7bec3b8fb91614d9567c5428cc0c4a7a
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-04-09 02:17:35 +0900

    fix : 타입 수정

--- src/1_Page/ChampionshipEdit/type.d.ts
+++ src/1_Page/ChampionshipEdit/type.d.ts
@@ -6,15 +6,11 @@ type ChampionshipFormValues = {
   championship_type_idx: number;
   championship_list_name: string;
   championship_list_description: string;
-  championship_list_throphy_img: File | undefined;
+  championship_list_throphy_img: (File | undefined)[];
   championship_list_color: string;
   championship_list_start_date: string;
   championship_list_end_date: string;
   participation_team_idxs: number[];
-  championship_award: {
-    championship_award_name: string;
-    file: File | undefined;
-  }[];
   community_list_idx: number;
 };
 
