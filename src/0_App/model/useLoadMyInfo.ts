234e5d74700628e14933d5634ecdf68a68cda4c4
RyuDongHo <thffhahs03@gmail.com>
2025-04-10 06:08:21 +0900

    fix: login 상태 유지 login 상태 유지를 위한 기능이 적용되어 있지 않았던 부분(오타)을 수정했고 access token 쿠키의 path와 maxAge설정 추가

---
+++ src/0_App/model/useLoadMyInfo.ts
@@ -0,0 +1,29 @@
+import React from "react";
+import useGetMyInfo from "../../3_Entity/Account/useGetMyInfo";
+import { useAuthStore } from "../../4_Shared/lib/useMyInfo";
+import { useCookies } from "react-cookie";
+
+const useLoadMyInfo = (): [MyInfo] => {
+  const [myInfo, loading] = useGetMyInfo();
+  const { login } = useAuthStore();
+  const [cookies] = useCookies(["access_token"]);
+
+  React.useEffect(() => {
+    if (!loading && myInfo) {
+      const { player_status, user_idx, profile_image, team_idx } = myInfo;
+      login({
+        playerStatus: player_status,
+        accessToken: cookies.access_token,
+        userIdx: user_idx,
+        communityRoleIdx: 0,
+        teamRoleIdx: 0,
+        teamIdx: team_idx,
+        profileImg: profile_image,
+      });
+    }
+  }, [myInfo, loading, login, cookies.access_token]);
+
+  return [myInfo];
+};
+
+export default useLoadMyInfo;
