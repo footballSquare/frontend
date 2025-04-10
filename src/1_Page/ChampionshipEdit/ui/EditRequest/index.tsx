e88fb198c609a125ae223156ce096f277bd6bd1c
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-04-10 18:36:53 +0900

    feat : 호출 구현

---
+++ src/1_Page/ChampionshipEdit/ui/EditRequest/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import useGetChampionshipEndData from "../../../../3_Entity/Championship/useGetChampionshipEndData";
+import useGetChampionshipInfo from "../../../../3_Entity/Championship/useGetChampionshipInfo";
+import useEditChampionshipStore from "../../../../4_Shared/zustand/useEditChampionshipStore";
+import { convertToChampionshipForm } from "./util/conver";
+
+const EditRequest = (props: EditRequestProps) => {
+  const { reset } = props;
+  const { championshipListIdx } = useEditChampionshipStore();
+
+  // 항상 숫자를 전달하기 위해 기본값 0 사용
+  const idx = championshipListIdx || 0;
+
+  // 훅은 조건부 호출하지 않고 항상 호출됨
+  const [championshipEndData] = useGetChampionshipEndData(idx);
+  const [championshipInfo] = useGetChampionshipInfo(idx);
+
+  React.useEffect(() => {
+    if (!championshipInfo || !championshipEndData) return;
+    reset(convertToChampionshipForm(championshipInfo, championshipEndData));
+  }, [championshipEndData, championshipInfo, reset]);
+
+  return null;
+};
+
+export default EditRequest;
