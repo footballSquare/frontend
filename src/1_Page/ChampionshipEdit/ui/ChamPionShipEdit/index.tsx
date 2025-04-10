1353fc715f377570ad8ed6aa0c01dab70adb1144
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-04-10 18:07:42 +0900

    feat : conver 구현

--- src/1_Page/ChampionshipEdit/ui/ChamPionShipEdit/index.tsx
+++ src/1_Page/ChampionshipEdit/ui/ChamPionShipEdit/index.tsx
@@ -1,2 +1,23 @@
-const ChampionshipEdit = () => {};
+import useGetChampionshipInfo from "../../../../3_Entity/Championship/useGetChampionshipInfo";
+import useGetChampionshipEndData from "../../../../3_Entity/Championship/useGetChampionshipEndData";
+import useEditChampionshipStore from "../../../../4_Shared/zustand/useEditChampionshipStore";
+import { convertToChampionshipForm } from "./util/convert";
+
+const ChampionshipEdit = (props: ChampionshipEditProps) => {
+  const { child } = props;
+  const { championshipListIdx } = useEditChampionshipStore();
+
+  const [championshipEndData] = useGetChampionshipEndData(
+    championshipListIdx || 0
+  );
+    const [championshipInfo] = useGetChampionshipInfo(championshipListIdx || 0);
+    
+    const defaultValues =  convertToChampionshipForm(championshipEndData,championshipInfo)
+
+    
+    
+    return (
+      
+  )
+};
 export default ChampionshipEdit;
