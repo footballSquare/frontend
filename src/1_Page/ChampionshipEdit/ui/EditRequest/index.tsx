3b11b50dd0e7ee5867c8b2a1f01831663285a321
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-04-10 19:10:21 +0900

    fix : edit 상태는 쿼리스트링 이용

--- src/1_Page/ChampionshipEdit/ui/EditRequest/index.tsx
+++ src/1_Page/ChampionshipEdit/ui/EditRequest/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 import useGetChampionshipEndData from "../../../../3_Entity/Championship/useGetChampionshipEndData";
 import useGetChampionshipInfo from "../../../../3_Entity/Championship/useGetChampionshipInfo";
 import { convertToChampionshipForm } from "./util/conver";
-import useParamInteger from "../../../../4_Shared/model/useParamInteger";
 
 const EditRequest = (props: EditRequestProps) => {
   const { reset } = props;
-  const championshipIdx = useParamInteger("championshipIdx");
+  const [searchParams] = useSearchParams();
+  const championshipIdx = Number(searchParams.get("championshipIdx") ?? 0);
 
-  // 훅은 조건부 호출하지 않고 항상 호출됨
   const [championshipEndData] = useGetChampionshipEndData(championshipIdx);
   const [championshipInfo] = useGetChampionshipInfo(championshipIdx);
 
