0fe27fc4286ca58f126307376fc87e70e558a801
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-04-10 17:48:55 +0900

    fix : 메모리 최적화 위한 idx만 넘기도록 수정

--- src/4_Shared/zustand/useEditChampionshipStore.ts
+++ src/4_Shared/zustand/useEditChampionshipStore.ts
@@ -2,19 +2,12 @@ import { create } from "zustand";
 
 type EditChampionshipStore = {
   championshipListIdx: number | null;
-  championshipInfo: ChampionshipInfo | null;
-  setChampionshipInfo: (info: ChampionshipInfo) => void;
   setChampionshipListIdx: (idx: number) => void;
-  clearSetChampionshipInfo: () => void;
 };
 
 const useEditChampionshipStore = create<EditChampionshipStore>((set) => ({
   championshipListIdx: null,
-  championshipInfo: null,
-  setChampionshipInfo: (info: ChampionshipInfo) =>
-    set({ championshipInfo: info }),
   setChampionshipListIdx: (idx: number) => set({ championshipListIdx: idx }),
-  clearSetChampionshipInfo: () => set({ championshipInfo: null }),
 }));
 
 export default useEditChampionshipStore;
