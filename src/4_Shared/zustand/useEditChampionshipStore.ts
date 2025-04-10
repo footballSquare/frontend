16fbb3345e8a3c5af04b4526e99414e3dd1b08b1
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-04-10 16:56:41 +0900

    feat : 전역 state 메모리 초기화 구현

--- src/4_Shared/zustand/useEditChampionshipStore.ts
+++ src/4_Shared/zustand/useEditChampionshipStore.ts
@@ -1,14 +1,16 @@
 import { create } from "zustand";
 
-interface EditChampionshipStore {
+type EditChampionshipStore = {
   championshipInfo: ChampionshipInfo | null;
   setChampionshipInfo: (info: ChampionshipInfo) => void;
-}
+  clearSetChampionshipInfo: () => void;
+};
 
 const useEditChampionshipStore = create<EditChampionshipStore>((set) => ({
   championshipInfo: null,
   setChampionshipInfo: (info: ChampionshipInfo) =>
     set({ championshipInfo: info }),
+  clearSetChampionshipInfo: () => set({ championshipInfo: null }),
 }));
 
 export default useEditChampionshipStore;
