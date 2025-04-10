79913661ee023f3a2a5ecc660cd5a0210363b96b
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-04-10 18:59:51 +0900

    fix : 종료시 전역 state 초기화

--- src/4_Shared/zustand/useEditChampionshipStore.ts
+++ src/4_Shared/zustand/useEditChampionshipStore.ts
@@ -2,12 +2,13 @@ import { create } from "zustand";
 
 type EditChampionshipStore = {
   championshipListIdx: number | null;
-  setChampionshipListIdx: (idx: number) => void;
+  setChampionshipListIdx: (idx: number | null) => void;
 };
 
 const useEditChampionshipStore = create<EditChampionshipStore>((set) => ({
   championshipListIdx: null,
-  setChampionshipListIdx: (idx: number) => set({ championshipListIdx: idx }),
+  setChampionshipListIdx: (idx: number | null) =>
+    set({ championshipListIdx: idx }),
 }));
 
 export default useEditChampionshipStore;
