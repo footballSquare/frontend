2b1cd4d53f9db5ead95c071790bf080a8d325a07
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-04-10 19:17:41 +0900

    fix: 성공시 대회 페이지로 이동 구현

--- src/1_Page/ChampionshipEdit/model/useManageServerState.ts
+++ src/1_Page/ChampionshipEdit/model/useManageServerState.ts
@@ -16,7 +16,12 @@ const useManageServerState = (
     switch (serverState.status) {
       case 200:
         alert(sucessMessage);
-        navigate(naviateUrl);
+        navigate(
+          `${naviateUrl}/${
+            (serverState.data as { championship_list_idx: number })
+              .championship_list_idx
+          }`
+        );
         break;
       case 409:
         alert("이미 존재하는 대회명입니다");
