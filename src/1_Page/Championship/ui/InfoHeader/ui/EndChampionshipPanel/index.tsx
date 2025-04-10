7e12e74808f1a41254446c0f04e66bc5a15d1873
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-04-10 17:44:43 +0900

    fix : 호출 최적화 위한 정보 넘겨주기 전역 state 구현

--- src/1_Page/Championship/ui/InfoHeader/ui/EndChampionshipPanel/index.tsx
+++ src/1_Page/Championship/ui/InfoHeader/ui/EndChampionshipPanel/index.tsx
@@ -1,15 +1,34 @@
 import React from "react";
 import useToggleState from "../../../../../../4_Shared/model/useToggleState";
 import EndChampionshipModal from "./ui/EndChampionshipModal";
+import useEditChampionshipStore from "../../../../../../4_Shared/zustand/useEditChampionshipStore";
+import useParamInteger from "../../../../../../4_Shared/model/useParamInteger";
+import { useNavigate } from "react-router-dom";
 
-const EndChampionshipPanel = () => {
+const EndChampionshipPanel = (props: EndChampionshipPanelProps) => {
+  const { championshipInfo } = props;
+  const navigate = useNavigate();
+  const championshipListIdx = useParamInteger("championshipIdx");
   const [isModalOpen, handleToggleModal] = useToggleState();
+  const { setChampionshipInfo, setChampionshipListIdx } =
+    useEditChampionshipStore();
 
   const cachedChampionshipEndDataRef = React.useRef<ChampionshipEndData>(
     {} as ChampionshipEndData
   );
   return (
     <div>
+      <button
+        className="px-3 py-1 text-sm border border-current rounded-md hover:bg-white/10 transition-colors"
+        onClick={() => {
+          setChampionshipListIdx(championshipListIdx);
+          setChampionshipInfo(championshipInfo);
+          navigate(
+            `/championship-edit/edit/${championshipInfo.community_list_idx}`
+          );
+        }}>
+        수정
+      </button>
       <button
         onClick={handleToggleModal}
         className="px-3 py-1 text-sm border border-current rounded-md hover:bg-white/10 transition-colors">
