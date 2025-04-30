b10a67b4ceaed4226c8eb3adf495cedf023246c0
RyuDongHo <thffhahs03@gmail.com>
2025-04-30 18:33:23 +0900

    chore: time checker time checker 오타 수정

--- src/4_Shared/lib/timeChecker.ts
+++ src/4_Shared/lib/timeChecker.ts
@@ -1,7 +1,7 @@
 export const isPastTime = (givenTime: string): boolean => {
   const currentTime = new Date();
   const targetTime = new Date(givenTime);
-  targetTime.setHours(targetTime.getHours() + 9); // UTC+9 시간대 적용 (KST)
+  targetTime.setHours(targetTime.getHours() - 9); // UTC+9 시간대 적용 (KST)
   
   return targetTime < currentTime; // 주어진 시간이 현재 시간보다 이전이면 true 반환
 };
