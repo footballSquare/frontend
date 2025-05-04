43f17c78399423e62506e119205fed04f2c3597f
RyuDongHo <thffhahs03@gmail.com>
2025-05-05 05:08:12 +0900

    fix: oauth api build error 문제 제거

--- src/1_Page/OAuthHub/model/useDiscord.ts
+++ src/1_Page/OAuthHub/model/useDiscord.ts
@@ -1,11 +1,14 @@
 import React from "react";
 import useGetDiscordSiginIn from "../../../3_Entity/Account/useGetDiscordSignIn";
 
-const useDiscord = () => {
+const useDiscord = (props: UseDiscordProps) => {
+  const { code, state } = props;
   const [getDiscordSiginIn] = useGetDiscordSiginIn();
   React.useEffect(() => {
-    getDiscordSiginIn({ code: code || "", state: state || "" });
-  }, []);
-}
+    if (code && state) {
+      getDiscordSiginIn({ code, state });
+    }
+  }, [code, state]);
+};
 
 export default useDiscord;
