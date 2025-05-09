e2f97d25f6076588a881281ca0fa89f5585a64d6
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-05-09 22:29:59 +0900

    refactor : BtnGroupManageModalPanel svg 요소 분리

--- src/1_Page/Team/ui/BtnGroupManageModalPanel/index.tsx
+++ src/1_Page/Team/ui/BtnGroupManageModalPanel/index.tsx
@@ -15,6 +15,11 @@ import ManageModal from "./ui/ManageModal";
 import useManagePutServerState from "./model/useManagePutServerState";
 import { useNavigate } from "react-router-dom";
 
+import exitIcon from "../../../../4_Shared/assets/svg/exit.svg";
+import joinIcon from "../../../../4_Shared/assets/svg/join.svg";
+import settingIcon from "../../../../4_Shared/assets/svg/setting.svg";
+import plusIcon from "../../../../4_Shared/assets/svg/plus.svg";
+
 const BtnGroupManageModalPanel = (props: BtnGroupManageModalPanelProps) => {
   const { teamInfo, handlers } = props;
   const naviagate = useNavigate();
@@ -70,20 +75,7 @@ const BtnGroupManageModalPanel = (props: BtnGroupManageModalPanelProps) => {
                 updateToLeave();
               }
             }}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4 mr-2"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
-              />
-            </svg>
-            팀 탈퇴
+            <img src={exitIcon} alt="팀 탈퇴" className="h-4 w-4 mr-2" />팀 탈퇴
           </button>
         ) : (
           <button
@@ -99,20 +91,7 @@ const BtnGroupManageModalPanel = (props: BtnGroupManageModalPanelProps) => {
                 updateToSignPending();
               }
             }}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4 mr-2"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"
-              />
-            </svg>
-            팀 가입
+            <img src={joinIcon} alt="팀 가입" className="h-4 w-4 mr-2" />팀 가입
           </button>
         )}
       </div>
@@ -124,25 +103,7 @@ const BtnGroupManageModalPanel = (props: BtnGroupManageModalPanelProps) => {
           <button
             className="bg-gray-800 hover:bg-gray-700 text-gray-200 text-sm font-medium py-2 px-4 rounded-lg border border-gray-700 shadow-md transition flex items-center"
             onClick={handleToggleManageModal}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4 mr-1.5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
-              />
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-              />
-            </svg>
+            <img src={settingIcon} alt="팀 관리" className="h-4 w-4 mr-1.5" />
             팀관리
           </button>
         )}
@@ -152,19 +113,7 @@ const BtnGroupManageModalPanel = (props: BtnGroupManageModalPanelProps) => {
           <button
             className="bg-indigo-700 hover:bg-indigo-600 text-white text-sm font-medium py-2 px-4 rounded-lg shadow-md transition flex items-center"
             onClick={toggleMakeMatchModal}>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-4 w-4 mr-1.5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor">
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
-              />
-            </svg>
+            <img src={plusIcon} alt="매치 생성" className="h-4 w-4 mr-1.5" />
             매치 생성
           </button>
         )}
