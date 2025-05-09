89de8a66d2659825c1ad70d8c35747e6e06c02c3
RyuDongHo <thffhahs03@gmail.com>
2025-05-09 13:28:35 +0900

    chore: hookForm schema hookForm 스키마 수정

--- src/4_Shared/hookForm/PostBoardInput/schema.ts
+++ src/4_Shared/hookForm/PostBoardInput/schema.ts
@@ -3,7 +3,7 @@ import * as yup from "yup";
 const postBoardInputSchema = yup.object().shape({
   title: yup.string().required("제목을 입력해주세요."),
   content: yup.string().required("내용을 입력해주세요."),
-  file: yup.mixed<File>().required("이미지를 선택해주세요."),
+  file: yup.mixed<File>().nullable(),
 });
 
 export default postBoardInputSchema;
