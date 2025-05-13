70cfda9a418300bc6e354114ffcbb6ca41a77b79
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-05-13 22:42:29 +0900

    feat : PostDetail 잘못된 페이지 접속 처리 했습니다

--- src/3_Entity/Board/useGetBoardDetail.ts
+++ src/3_Entity/Board/useGetBoardDetail.ts
@@ -1,11 +1,13 @@
 import React from "react";
 import { useFetchData } from "../../4_Shared/util/apiUtil";
+import { useNavigate } from "react-router-dom";
 
 const useGetBoardDetail = (board_list_idx: number): [BoardDetails, boolean] => {
   const [serverState, request, loading] = useFetchData();
   const [boardDetail, setBoardDetail] = React.useState<BoardDetails>(
     {} as BoardDetails
   );
+  const navigate = useNavigate();
 
   React.useEffect(() => {
     const endpoint = `/board/${board_list_idx}`;
@@ -19,6 +21,18 @@ const useGetBoardDetail = (board_list_idx: number): [BoardDetails, boolean] => {
       setBoardDetail(nestedBoard);
     }
   }, [loading, serverState]);
+
+  React.useEffect(() => {
+    if (!serverState) return;
+    switch (serverState.status) {
+      case 200:
+        return;
+
+      default:
+        alert("게시글을 불러오는 데 실패했습니다.");
+        navigate(`/404`);
+    }
+  }, [serverState]);
 
   return [boardDetail, loading];
 };
