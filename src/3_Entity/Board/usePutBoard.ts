22ea186dd38ebaa27f6a59e24de1ca8ce3ed1258
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-05-14 02:15:21 +0900

    feat : usePutBoard 성공 후 리디렉트 구현

--- src/3_Entity/Board/usePutBoard.ts
+++ src/3_Entity/Board/usePutBoard.ts
@@ -1,14 +1,31 @@
+import React from "react";
 import { useFetchData } from "../../4_Shared/util/apiUtil";
+import { useNavigate } from "react-router-dom";
 
 const usePutBoard = (
   boardListIdx: number
 ): [(data: PostEditFormFields) => void, Record<string, unknown> | null] => {
   const [serverState, request] = useFetchData();
+  const navigate = useNavigate();
 
   const putBoard = (data: PostEditFormFields) => {
     const endPoint = `/board/${boardListIdx}`;
     request("PUT", endPoint, data, true);
   };
+
+  React.useEffect(() => {
+    if (!serverState) return;
+    switch (serverState.status) {
+      case 200: {
+        navigate(`/post/${boardListIdx}`);
+        break;
+      }
+      default: {
+        alert("게시글을 수정하는데 실패했습니다.");
+        break;
+      }
+    }
+  }, [serverState]);
 
   return [putBoard, serverState];
 };
