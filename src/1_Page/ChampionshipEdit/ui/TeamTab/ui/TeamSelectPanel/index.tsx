0779b1161b4482540080341bb408ea3543ecb0fd
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-05-13 17:16:06 +0900

    style :TeamSelectPanel grass 색상 사용하도록 수정"

--- src/1_Page/ChampionshipEdit/ui/TeamTab/ui/TeamSelectPanel/index.tsx
+++ src/1_Page/ChampionshipEdit/ui/TeamTab/ui/TeamSelectPanel/index.tsx
@@ -21,7 +21,7 @@ const TeamSelectPanel = (props: TeamSelectPanelProps) => {
     <div>
       <button
         type="button"
-        className="px-4 py-2.5 bg-blue-600 hover:bg-blue-700 text-white rounded-lg transition-colors duration-150 flex items-center shadow-md"
+        className="px-4 py-2.5 bg-grass-600 hover:bg-grass-700 text-white rounded-lg transition-colors duration-150 flex items-center shadow-md"
         onClick={toggleModal}>
         팀 선택
       </button>
