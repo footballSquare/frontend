dde8d45956989640e5290d78cf5e99de5b51714c
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-05-18 02:34:38 +0900

    feat : useGetTeamInfo 팀 없는 경우 navigate 구현했습니다

--- src/3_Entity/Team/useGetTeamInfo.ts
+++ src/3_Entity/Team/useGetTeamInfo.ts
@@ -1,10 +1,12 @@
 import React from "react";
 import { useFetchData } from "../../4_Shared/util/apiUtil";
+import { useNavigate } from "react-router-dom";
 
 const useGetTeamInfo = (teamListIdx: number): [TeamInfo, boolean] => {
   const [serverState, request, loading] = useFetchData();
   const [teamInfo, setTeamInfo] = React.useState<TeamInfo>({} as TeamInfo);
 
+  const navigate = useNavigate();
   React.useEffect(() => {
     const endPoint = `/team/${teamListIdx}/information`;
     request("GET", endPoint, null, true);
@@ -15,6 +17,23 @@ const useGetTeamInfo = (teamListIdx: number): [TeamInfo, boolean] => {
       setTeamInfo((serverState as { team: TeamInfo }).team);
     }
   }, [loading, serverState]);
+
+  React.useEffect(() => {
+    if (!serverState) return;
+
+    switch (serverState.status) {
+      case 200:
+        return;
+      case 404:
+        alert("존재하지 않는 팀입니다.");
+        navigate(-1);
+        return;
+      default:
+        alert("서버 오류입니다.");
+        navigate(-1);
+        return;
+    }
+  }, [serverState]);
 
   return [teamInfo, loading];
 };
