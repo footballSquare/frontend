739efb7b6b6fd1017d91e095b01f687f4faceb37
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-05-23 20:48:06 +0900

    feat : useProfileDashBoardHookForm 리턴 타입 명시했습니다

--- src/1_Page/Profile/model/useProfileDashBoardHookForm.ts
+++ src/1_Page/Profile/model/useProfileDashBoardHookForm.ts
@@ -3,12 +3,13 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { profileDashBoardInputSchema } from "../../../4_Shared/hookForm/ProfileDashBoardInput/schema";
 
-const useProfileDashBoardHookform = (initUserInfo: UserInfo) => {
+const useProfileDashBoardHookform = (
+  initUserInfo: UserInfo
+): UseProfileDashBoardHookformReturn => {
   const form = useForm<UserInfoForm>({
     resolver: yupResolver(profileDashBoardInputSchema),
     mode: "onChange",
   });
-
   const { reset, watch } = form;
 
   const nickname = watch("nickname");
