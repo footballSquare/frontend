d78877339faa35819b12df539db5c8f966305ef9
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-05-25 01:59:43 +0900

    refactor : useProfileDashBoardHookForm 불필요하게 분리된 함수 합치도록 수정했습니다

--- src/1_Page/Profile/model/useProfileDashBoardHookForm.ts
+++ src/1_Page/Profile/model/useProfileDashBoardHookForm.ts
@@ -16,7 +16,14 @@ const useProfileDashBoardHookform = (
   const matchPositionIdx = watch("match_position_idx");
 
   React.useEffect(() => {
-    reset(convertToUserInfoForm(initUserInfo));
+    reset({
+      nickname: initUserInfo.nickname,
+      common_status_idx: initUserInfo.common_status_idx,
+      platform: initUserInfo.platform,
+      discord_tag: initUserInfo.discord_tag,
+      message: initUserInfo.message,
+      match_position_idx: initUserInfo.match_position_idx,
+    });
   }, [initUserInfo]); // 초기값 설정
 
   return {
@@ -26,14 +33,3 @@ const useProfileDashBoardHookform = (
   };
 };
 export default useProfileDashBoardHookform;
-
-const convertToUserInfoForm = (userInfo: UserInfo): UserInfoForm => {
-  return {
-    nickname: userInfo.nickname,
-    common_status_idx: userInfo.common_status_idx,
-    platform: userInfo.platform,
-    discord_tag: userInfo.discord_tag,
-    message: userInfo.message,
-    match_position_idx: userInfo.match_position_idx,
-  };
-};
