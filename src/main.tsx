969cc8f23b72e4bcc479b915b6020f290dafbde4
RyuDongHo <thffhahs03@gmail.com>
2025-05-24 21:54:28 +0900

    chore: vercel analyics vercel analytics 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import { createRoot } from "react-dom/client";
 import { CookiesProvider } from "react-cookie";
+import { Analytics } from '@vercel/analytics/react'
 import App from "./0_App";
 
 createRoot(document.getElementById("root")!).render(
   <CookiesProvider>
+    <Analytics />
     <App />
   </CookiesProvider>
 );
