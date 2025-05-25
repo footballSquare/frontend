f354d4327652c814254be6ebea6f5191b24ca223
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-05-26 00:45:17 +0900

    refactor : entity 팀 관련 엔티티 요소 status return 하도록 수정했습니다

--- src/3_Entity/Team/useDeleteLeaveTeam.ts
+++ src/3_Entity/Team/useDeleteLeaveTeam.ts
@@ -3,15 +3,15 @@ import { useFetchData } from "../../4_Shared/util/apiUtil";
 const useDeleteLeaveTeam = (
   teamListIdx: number
 ): [
-  deleteLeaveTeam: () => void,
+  deleteLeaveTeam: () => Promise<number | undefined>,
   serverState: Record<string, unknown> | null,
   loading: boolean
 ] => {
   const [serverState, request, loading] = useFetchData();
 
-  const deleteLeaveTeam = () => {
+  const deleteLeaveTeam = async () => {
     const endPoint = `/team/${teamListIdx}/leave`;
-    request("DELETE", endPoint, null, true);
+    return await request("DELETE", endPoint, null, true);
   };
 
   return [deleteLeaveTeam, serverState, loading];
--- src/3_Entity/Team/usePutTeamEmblem.ts
+++ src/3_Entity/Team/usePutTeamEmblem.ts
@@ -4,22 +4,29 @@ import { useFetchData } from "../../4_Shared/util/apiUtil";
 const usePutTeamEmblem = (
   teamListIdx: number
 ): [
-  putTeamEmblem: (file: File) => void,
+  putTeamEmblem: (file: File) => Promise<number | undefined>,
   serverState: unknown,
   loading: boolean
 ] => {
   const [serverState, request, loading] = useFetchData();
 
-  const putTeamEmblem = (file: File) => {
+  const putTeamEmblem = async (file: File) => {
     const endPoint = `/team/${teamListIdx}/emblem`;
     const formData = new FormData();
     formData.append("file", file);
-    request("PUT", endPoint, formData, true);
+    return await request("PUT", endPoint, formData, true);
   };
 
   React.useEffect(() => {
     if (!serverState) return;
-    if (serverState.status === 403) return;
+    switch (serverState.status) {
+      case 403:
+        alert("팀원 수 부족: 최소 10명 필요 (현재 1명)");
+        break;
+      default:
+        alert("잘못된 접근입니다.");
+        break;
+    }
   }, [serverState]);
 
   return [putTeamEmblem, serverState, loading];
