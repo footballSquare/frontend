0a55a1c2fbde7334e6043f164adabdc91b6e14ff
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-05-27 15:03:50 +0900

    feat : DefaultTeamProfile 팀 프로필이미지 없는 경우를 위한 기본 팀이미지 통일을 위해 분리했습니다

---
+++ src/4_Shared/components/DefaultTeamProfile/index.tsx
@@ -0,0 +1,22 @@
+const DefaultTeamProfile = (props: DefaultTeamProfileProps) => {
+  const {
+    team_list_color = "#3182f6",
+    team_list_short_name = "팀",
+    width = 48,
+    height = 48,
+  } = props;
+
+  return (
+    <div
+      className={`rounded-full mr-3 flex items-center justify-center text-white font-bold text-lg`}
+      style={{
+        backgroundColor: team_list_color,
+        width: `${width}px`,
+        height: `${height}px`,
+      }}>
+      {team_list_short_name.slice(0, 3)}
+    </div>
+  );
+};
+
+export default DefaultTeamProfile;
---
+++ src/4_Shared/components/DefaultTeamProfile/type.d.ts
@@ -0,0 +1,6 @@
+type DefaultTeamProfileProps = {
+  team_list_color?: string;
+  team_list_short_name?: string;
+  width?: number;
+  height?: number;
+};
