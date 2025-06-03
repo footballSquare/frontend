a4cac8534c8bc885002196c0c9b2c35bc8aebabc
RyuDongHo <81012973+RyuDongHo@users.noreply.github.com>
2025-06-04 01:51:00 +0900

    chore: socket server
    server url 변경

--- src/1_Page/ChatTesting/index.tsx
+++ src/1_Page/ChatTesting/index.tsx
@@ -4,7 +4,7 @@ import { useCookies } from "react-cookie";
 import io, { Socket } from "socket.io-client";
 import React from "react";
 
-const API_URL = import.meta.env.VITE_API_URL; // API URL
+const API_URL = import.meta.env.VITE_SERVER_URL; // API URL
 
 interface ChatMessage {
   sender_idx: number;
