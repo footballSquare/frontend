239d6a51198e60ef514dd8b7161d4d6953819ef7
RyuDongHo <thffhahs03@gmail.com>
2025-06-04 16:37:27 +0900

    fix: team match team match 생성시 새로고침 되도록 수정

--- src/3_Entity/Match/usePostTeamMatch.ts
+++ src/3_Entity/Match/usePostTeamMatch.ts
@@ -1,3 +1,4 @@
+import React from "react";
 import { useFetchData } from "../../4_Shared/util/apiUtil";
 
 const usePostTeamMatch = (
@@ -14,6 +15,17 @@ const usePostTeamMatch = (
     const endPoint = `/match/team/${teamIdx}`;
     request("POST", endPoint, props, true);
   };
+
+  React.useEffect(() => {
+    if (!loading && serverState) {
+      if (serverState.status === 200) {
+        alert("매치가 생성되었습니다.");
+        window.location.reload();
+      } else {
+        alert("매치 생성에 실패했습니다.");
+      }
+    }
+  }, [loading, serverState]);
 
   return [postTeamMatch, serverState, loading];
 };
