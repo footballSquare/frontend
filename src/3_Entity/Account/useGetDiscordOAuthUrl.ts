89e0c233f66905ed764f485c8b8bca79c5c8f6ae
RyuDongHo <thffhahs03@gmail.com>
2025-06-20 20:11:06 +0900

    feat: 자동 로그인 설정 추가 자동 로그인을 유저가 선택 할 수 있도록 변경

--- src/3_Entity/Account/useGetDiscordOAuthUrl.ts
+++ src/3_Entity/Account/useGetDiscordOAuthUrl.ts
@@ -1,15 +1,23 @@
 import React from "react";
 import { useFetchData } from "../../4_Shared/util/apiUtil.ts";
 
-const useGetDiscordOAuthUrl = (): [DiscordOAuthUrl, boolean] => {
+const useGetDiscordOAuthUrl = (
+  props: UseGetDiscordOAuthUrlProps
+): [DiscordOAuthUrl, boolean] => {
+  const { signInPersist, deviceUUID } = props;
   const [serverState, request, loading] = useFetchData();
   const [discordOAuthUrl, setDiscordOAuthUrl] = React.useState<DiscordOAuthUrl>(
     {} as DiscordOAuthUrl
   );
 
   React.useEffect(() => {
-    request("GET", "/account/oauth/url/discord", null, false);
-  }, [request]);
+    request(
+      "GET",
+      `/account/oauth/url/discord/?persistent=${signInPersist}&device_uuid=${deviceUUID}`,
+      null,
+      false
+    );
+  }, [request, signInPersist, deviceUUID]);
 
   React.useEffect(() => {
     if (!loading && serverState) {
