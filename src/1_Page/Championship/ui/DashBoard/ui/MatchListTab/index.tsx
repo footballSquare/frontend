e2114164028d744c9cf9e4c9535ad8e9a7f4d961
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2025-07-04 14:00:07 +0900

    refactor : MatchListTab 스탯입력하기 버튼 문구 매치 상세보기로 수정했습니다 내팀의 경우에만 출력되도록 수정했습니다

--- src/1_Page/Championship/ui/DashBoard/ui/MatchListTab/index.tsx
+++ src/1_Page/Championship/ui/DashBoard/ui/MatchListTab/index.tsx
@@ -132,7 +132,7 @@ const MatchListTab = (props: MatchListTabProps) => {
                   {/* 매치 선택시 */}
                   {/* 상단 탭 네비게이션 */}
                   <nav className="flex flex-col md:flex-row justify-between items-start md:items-center mb-4 gap-3 lg:mb-6 lg:gap-4">
-                    {/* 스크롤 가능한 탭 버튼 목록 */}
+                    {/* 탭 버튼 목록 (라인업 , 팀기록 , 개인기록 ) */}
                     <div className="flex overflow-x-auto space-x-2 p-1 rounded-lg scrollbar-hide w-full lg:w-auto lg:p-2 lg:rounded-md">
                       {VIEW_MODE_BUTTONS.map(({ id, label }) => (
                         <button
@@ -143,6 +143,7 @@ const MatchListTab = (props: MatchListTabProps) => {
                               ? "bg-blue-600 text-white"
                               : "bg-gray-700 text-gray-200 hover:bg-gray-600"
                           }`}
+                          // 해당 버튼 활성화 상태일때 대회색상 OR bg-blue 그 외의 경우 회색
                           style={
                             viewMode === id
                               ? { backgroundColor: championshipListColor }
@@ -153,14 +154,15 @@ const MatchListTab = (props: MatchListTabProps) => {
                       ))}
                     </div>
 
-                    {/* 상세 보기 버튼 */}
+                    {/* 매치 클릭된 경우*/}
+                    {/* 내 팀인경우에만 매치 모달출력 */}
                     {selectedMatch &&
                       firstTeamMatchInfo &&
                       secondTeamMatchInfo && (
                         <div className="flex flex-col sm:flex-row gap-2 w-full md:w-auto">
                           {firstTeamMatchInfo.team_list_idx === myTeamIdx && (
                             <button
-                              className="px-4 py-2 rounded-lg text-sm font-semibold border-2 transition-colors duration-200 w-full"
+                              className="min-w-[200px] px-4 py-2 rounded border-2"
                               style={{
                                 borderColor: firstTeamMatchInfo.team_list_color,
                               }}
@@ -168,13 +170,13 @@ const MatchListTab = (props: MatchListTabProps) => {
                                 setMatchIdx(firstTeamMatchInfo.match_match_idx);
                                 toggleMatchModal();
                               }}>
-                              {firstTeamMatchInfo.team_list_name}{" "}
-                              {"스탯을 입력하세요"}
+                              {firstTeamMatchInfo.team_list_name}
+                              <div>매치 상세보기</div>
                             </button>
                           )}
                           {secondTeamMatchInfo.team_list_idx === myTeamIdx && (
                             <button
-                              className="px-4 py-2 rounded-lg text-sm font-semibold border-2 transition-colors duration-200 w-full"
+                              className="min-w-[200px] px-4 py-2 rounded border-2"
                               style={{
                                 borderColor:
                                   secondTeamMatchInfo.team_list_color,
@@ -185,8 +187,8 @@ const MatchListTab = (props: MatchListTabProps) => {
                                 );
                                 toggleMatchModal();
                               }}>
-                              {secondTeamMatchInfo.team_list_name}{" "}
-                              {"스탯을 입력하세요"}
+                              {secondTeamMatchInfo.team_list_name}
+                              <div>매치 상세보기</div>
                             </button>
                           )}
                         </div>
